cae4ed79f1d4e73c0f0f4634b9d751df5df1cf60
David Zíka <itscky.mc@gmail.com>
2025-01-21 09:04:26 +0100

    David Zíka edition

--- script.js
+++ script.js
@@ -5,14 +5,21 @@ const pocetMat = document.getElementById("pocetMat");
 
 // kód vykreslení obrázků
 const pat = "<img src='img/pat.png'>";
+const mat = "<img src='img/mat.png'>";
 
 let kodFigurek = "";
 let i=0;
 while (i<10) {
-  i++;
-  kodFigurek += "<div>" + pat + "</div>";
+  if (i % 2 === 0) {
+    i++;
+    kodFigurek += "<div>" + pat + mat + "</div>";
+  }
+  else {
+    i++;
+    kodFigurek += "<div>" + mat + pat + "</div>";
+  }
 }
 figurky.innerHTML = kodFigurek; // vloží kód řady obrázků, kterou požadujeme vykreslit
 pocetPat.innerText = i;         // vypíše počet vykreslených figurek Pata (všech deset)
-pocetMat.innerText = 0;         // vypíše počet vykreslenách figurek Mata (žádný)
+pocetMat.innerText = i;         // vypíše počet vykreslenách figurek Mata (žádný)
 
